3318e18e3b67593a4e2dd90bde65e288eaa53dde
dan-zx <danyboy_shaggy@hotmail.com>
2015-02-25 13:01:11 -0600

    Marker icons are now loaded async.

--- src/main/java/com/grayfox/android/activity/RouteDisplayingActivity.java
+++ src/main/java/com/grayfox/android/activity/RouteDisplayingActivity.java
@@ -2,18 +2,24 @@ package com.grayfox.android.activity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.os.PersistableBundle;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.PolylineOptions;
 
@@ -21,6 +27,9 @@ import com.grayfox.android.R;
 import com.grayfox.android.client.model.Location;
 import com.grayfox.android.client.model.Poi;
 import com.grayfox.android.client.model.Recommendation;
+
+import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Target;
 
 import roboguice.activity.RoboActionBarActivity;
 import roboguice.inject.ContentView;
@@ -55,10 +64,13 @@ public class RouteDisplayingActivity extends RoboActionBarActivity implements On
     public void onMapReady(GoogleMap googleMap) {
         if (recommendation.getPoiSequence().length > 0) {
             for (Poi poi : recommendation.getPoiSequence()) {
-                // TODO: Fetch category icon from web
-                googleMap.addMarker(new MarkerOptions()
+                Marker marker = googleMap.addMarker(new MarkerOptions()
                         .position(new LatLng(poi.getLocation().getLatitude(), poi.getLocation().getLongitude()))
                         .title(poi.getName()));
+                Picasso.with(this)
+                        .load(poi.getCategories()[0].getIconUrl())
+                        .placeholder(R.drawable.ic_generic_category)
+                        .into(new PicassoMarker(marker, this));
             }
         }
         if (recommendation.getRoutePoints().length > 0) {
@@ -72,7 +84,7 @@ public class RouteDisplayingActivity extends RoboActionBarActivity implements On
         googleMap.addMarker(new MarkerOptions()
                 .position(latLngOrigin)
                 .title(getString(R.string.your_location))
-                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE)));
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_CYAN)));
         googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLngOrigin, 13f));
     }
 
@@ -83,6 +95,56 @@ public class RouteDisplayingActivity extends RoboActionBarActivity implements On
                 onBackPressed();
                 return true;
             default: return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private static class PicassoMarker implements Target {
+
+        private Marker marker;
+        private View layout;
+
+        private PicassoMarker(Marker marker, Context context) {
+            this.marker = marker;
+            layout = ((LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE)).inflate(R.layout.category_marker, null);
+        }
+
+        @Override
+        public void onBitmapLoaded(Bitmap bitmap, Picasso.LoadedFrom from) {
+            marker.setIcon(BitmapDescriptorFactory.fromBitmap(iconToLayoutBitmap(bitmap)));
+        }
+
+        @Override
+        public void onBitmapFailed(Drawable errorDrawable) {
+            marker.setIcon(BitmapDescriptorFactory.fromBitmap(iconToLayoutBitmap(errorDrawable)));
+        }
+
+        @Override
+        public void onPrepareLoad(Drawable placeHolderDrawable) {
+            marker.setIcon(BitmapDescriptorFactory.fromBitmap(iconToLayoutBitmap(placeHolderDrawable)));
+        }
+
+        private Bitmap iconToLayoutBitmap(Drawable iconDrawable) {
+            ImageView categoryImageView = (ImageView) layout.findViewById(R.id.category_image);
+            categoryImageView.setImageDrawable(iconDrawable);
+            layout.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+            layout.measure(View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED), View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED));
+            layout.layout(0, 0, layout.getMeasuredWidth(), layout.getMeasuredHeight());
+            Bitmap bitmap = Bitmap.createBitmap(layout.getMeasuredWidth(), layout.getMeasuredHeight(), Bitmap.Config.ARGB_8888);
+            Canvas c = new Canvas(bitmap);
+            layout.draw(c);
+            return bitmap;
+        }
+
+        private Bitmap iconToLayoutBitmap(Bitmap iconBitmap) {
+            ImageView categoryImageView = (ImageView) layout.findViewById(R.id.category_image);
+            categoryImageView.setImageBitmap(iconBitmap);
+            layout.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+            layout.measure(View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED), View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED));
+            layout.layout(0, 0, layout.getMeasuredWidth(), layout.getMeasuredHeight());
+            Bitmap bitmap = Bitmap.createBitmap(layout.getMeasuredWidth(), layout.getMeasuredHeight(), Bitmap.Config.ARGB_8888);
+            Canvas c = new Canvas(bitmap);
+            layout.draw(c);
+            return bitmap;
         }
     }
 }
